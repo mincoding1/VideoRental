5d59b5160b94aa549d6b8f48c72393e595215d6b
mincoding <74457149+mincoding1@users.noreply.github.com>
2023-03-26 00:56:34 +0900

    Add files via upload

---
+++ java/src/test/java/movierental/CustomerTest.java
@@ -0,0 +1,32 @@
+package movierental;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+
+import org.junit.Test;
+
+public class CustomerTest {
+
+    @Test
+    public void test() {
+        Customer customer = new Customer("Bob");
+        customer.addRental(new Rental(new Movie("Jaws", Movie.REGULAR), 2));
+        customer.addRental(new Rental(new Movie("Golden Eye", Movie.REGULAR), 3));
+        customer.addRental(new Rental(new Movie("Short New", Movie.NEW_RELEASE), 1));
+        customer.addRental(new Rental(new Movie("Long New", Movie.NEW_RELEASE), 2));
+        customer.addRental(new Rental(new Movie("Bambi", Movie.CHILDRENS), 3));
+        customer.addRental(new Rental(new Movie("Toy Story", Movie.CHILDRENS), 4));
+
+        String expected = "" +
+                "Rental Record for Bob\n" +
+                "\tJaws\t2.0\n" +
+                "\tGolden Eye\t3.5\n" +
+                "\tShort New\t3.0\n" +
+                "\tLong New\t6.0\n" +
+                "\tBambi\t1.5\n" +
+                "\tToy Story\t3.0\n" +
+                "Amount owed is 19.0\n" +
+                "You earned 7 frequent renter points";
+
+        assertEquals(expected, customer.statement());
+    }
+}
