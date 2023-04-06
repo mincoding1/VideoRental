2602b84a6943366f87b86fede7ad1f37dd18a99d
mincoding <74457149+mincoding1@users.noreply.github.com>
2023-04-06 23:27:33 +0900

    Create Movie.cpp

---
+++ cpp/Movie.cpp
@@ -0,0 +1,35 @@
+#pragma once
+#include <string>
+
+class Movie
+{
+public:
+	static const int CHILDRENS = 2;
+	static const int NEW_RELEASE = 1;
+	static const int REGULAR = 0;
+
+	Movie(std::string& title, int price_code)
+		: title(title),
+		  priceCode(price_code)
+	{
+	}
+
+	int getPriceCode() 
+	{
+		return priceCode;
+	}
+
+	void setPriceCode(int price_code)
+	{
+		priceCode = price_code;
+	}
+
+	std::string getTitle() 
+	{
+		return title;
+	}
+
+private:
+	std::string title;
+	int priceCode;
+};
