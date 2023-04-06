581a0fffe2aba02b55e42cdbebf2d1596148bcaa
mincoding <74457149+mincoding1@users.noreply.github.com>
2023-04-06 23:27:44 +0900

    Create Rental.cpp

---
+++ cpp/Rental.cpp
@@ -0,0 +1,26 @@
+#pragma once
+#include "Movie.cpp";
+
+class Rental
+{
+public:
+	Rental(Movie& movie, int days_rented)
+		: movie(movie),
+		  daysRented(days_rented)
+	{
+	}
+
+	int getDaysRented() 
+	{
+		return daysRented;
+	}
+
+	Movie getMovie() 
+	{
+		return movie;
+	}
+
+private:
+	Movie movie;
+	int daysRented;
+};
