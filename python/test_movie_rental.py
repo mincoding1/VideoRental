6bedb6b9b68463263f09e1823f7fc941a583d96c
mincoding <74457149+mincoding1@users.noreply.github.com>
2023-05-08 09:53:23 +0900

    Update test_movie_rental.py

--- python/test_movie_rental.py
+++ python/test_movie_rental.py
@@ -1,23 +1,24 @@
-from movie_rental import Customer, Rental, Movie
+from unittest import TestCase
+from customer import Customer, Rental, Movie
 
+class Tests(TestCase):
+    def test_sample(self):
+        customer = Customer("Bob")
+        customer.add_rental(Rental(Movie("Jaws", Movie.REGULAR), 2))
+        customer.add_rental(Rental(Movie("Golden Eye", Movie.REGULAR), 3))
+        customer.add_rental(Rental(Movie("Short New", Movie.NEW_RELEASE), 1))
+        customer.add_rental(Rental(Movie("Long New", Movie.NEW_RELEASE), 2))
+        customer.add_rental(Rental(Movie("Bambi", Movie.CHILDRENS), 3))
+        customer.add_rental(Rental(Movie("Toy Story", Movie.CHILDRENS), 4))
 
-def tests():
-    customer = Customer("Bob")
-    customer.addRental(Rental(Movie("Jaws", Movie.REGULAR), 2))
-    customer.addRental(Rental(Movie("Golden Eye", Movie.REGULAR), 3))
-    customer.addRental(Rental(Movie("Short New", Movie.NEW_RELEASE), 1))
-    customer.addRental(Rental(Movie("Long New", Movie.NEW_RELEASE), 2))
-    customer.addRental(Rental(Movie("Bambi", Movie.CHILDRENS), 3))
-    customer.addRental(Rental(Movie("Toy Story", Movie.CHILDRENS), 4))
+        expected = "Rental Record for Bob\n"
+        expected += "\tJaws\t2.0\n"
+        expected += "\tGolden Eye\t3.5\n"
+        expected += "\tShort New\t3.0\n"
+        expected += "\tLong New\t6.0\n"
+        expected += "\tBambi\t1.5\n"
+        expected += "\tToy Story\t3.0\n"
+        expected += "Amount owed is 19.0\n"
+        expected += "You earned 7 frequent renter points"
 
-    expected = "Rental Record for Bob\n"
-    expected += "\tJaws\t2.0\n"
-    expected += "\tGolden Eye\t3.5\n"
-    expected += "\tShort New\t3.0\n"
-    expected += "\tLong New\t6.0\n"
-    expected += "\tBambi\t1.5\n"
-    expected += "\tToy Story\t3.0\n"
-    expected += "Amount owed is 19.0\n"
-    expected += "You earned 7 frequent renter points"
-
-    assert expected == customer.statement()
+        self.assertEqual(expected, customer.statement())
