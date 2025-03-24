fac2170dc3b1bcca675129ceb3301ea52622a67b
mincoding <74457149+mincoding1@users.noreply.github.com>
2025-03-24 13:11:49 +0900

    Update Movie.cpp

--- cpp/Movie.cpp
+++ cpp/Movie.cpp
@@ -3,21 +3,15 @@
 
 using std::string;
 
-class Movie
-{
+class Movie {
 public:
 	static const int CHILDRENS = 2;
 	static const int NEW_RELEASE = 1;
 	static const int REGULAR = 0;
 
-	Movie(const string& title, int price_code)
-		: title(title),
-		priceCode(price_code)
-	{
-	}
+	Movie(const string& title, int price_code) : title{ title }, priceCode{ price_code } {}
 
-	int getPriceCode()
-	{
+	int getPriceCode() {
 		return priceCode;
 	}
 
@@ -26,8 +20,7 @@ public:
 		priceCode = price_code;
 	}
 
-	string getTitle()
-	{
+	string getTitle() const {
 		return title;
 	}
 
