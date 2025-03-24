181a256bc72414d2ffa58f760b5acea573b881aa
mincoding <74457149+mincoding1@users.noreply.github.com>
2025-03-24 13:16:38 +0900

    Update customer_test.cpp

--- cpp/customer_test.cpp
+++ cpp/customer_test.cpp
@@ -2,24 +2,25 @@
 #include "customer.cpp"
 
 TEST(TestCaseName, TestName) {
-    Customer customer = Customer(string("Bob"));
-    customer.addRental(Rental(Movie(string("Jaws"), Movie::REGULAR), 2));
-    customer.addRental(Rental(Movie(string("Golden Eye"), Movie::REGULAR), 3));
-    customer.addRental(Rental(Movie(string("Short New"), Movie::NEW_RELEASE), 1));
-    customer.addRental(Rental(Movie(string("Long New"), Movie::NEW_RELEASE), 2));
-    customer.addRental(Rental(Movie(string("Bambi"), Movie::CHILDRENS), 3));
-    customer.addRental(Rental(Movie(string("Toy Story"), Movie::CHILDRENS), 4));
+    Customer customer = Customer(string{ "Bob" });
+
+    customer.addRental(Rental{ Movie{ string{ "Jaws" }, Movie::REGULAR }, 2 });
+    customer.addRental(Rental{ Movie{ string{ "Golden Eye" }, Movie::REGULAR }, 3 });
+    customer.addRental(Rental{ Movie{ string{ "Short New" }, Movie::NEW_RELEASE }, 1 });
+    customer.addRental(Rental{ Movie{ string{ "Long New" }, Movie::NEW_RELEASE }, 2 });
+    customer.addRental(Rental{ Movie{ string{ "Bambi" }, Movie::CHILDRENS }, 3 });
+    customer.addRental(Rental{ Movie{ string{ "Toy Story" }, Movie::CHILDRENS }, 4 });
 
     string expected = "" +
-        string("Rental Record for Bob\n") +
-        string("\tJaws\t2.0\n") +
-        string("\tGolden Eye\t3.5\n") +
-        string("\tShort New\t3.0\n") +
-        string("\tLong New\t6.0\n") +
-        string("\tBambi\t1.5\n") +
-        string("\tToy Story\t3.0\n") +
-        string("Amount owed is 19.0\n") +
-        string("You earned 7 frequent renter points");
+        string{ "Rental Record for Bob\n" } +
+        string{ "\tJaws\t2.0\n" } +
+        string{ "\tGolden Eye\t3.5\n" } +
+        string{ "\tShort New\t3.0\n" } +
+        string{ "\tLong New\t6.0\n" } +
+        string{ "\tBambi\t1.5\n" } +
+        string{ "\tToy Story\t3.0\n" } +
+        string{ "Amount owed is 19.0\n" } +
+        string{ "You earned 7 frequent renter points" };
 
     EXPECT_EQ(expected, customer.statement());
 }
