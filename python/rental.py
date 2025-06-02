f9d00d732557a69deb9498900e4c9cf5b798cde5
mincoding <74457149+mincoding1@users.noreply.github.com>
2025-06-03 00:18:24 +0900

    Update rental.py

--- python/rental.py
+++ python/rental.py
@@ -1,14 +1,12 @@
 from movie import Movie
 
 class Rental:
-    def __init__(self, movie: Movie, days_rented: int):
-        self._days_rented = days_rented
-        self._movie = movie
+    def __init__(self, movie: Movie, daysRented: int):
+        self.__days_rented = daysRented
+        self.__movie = movie
 
-    @property
-    def days_rented(self) -> int:
-        return self._days_rented
+    def get_days_rented(self):
+        return self.__days_rented
 
-    @property
-    def movie(self) -> Movie:
-        return self._movie
+    def get_movie(self):
+        return self.__movie
