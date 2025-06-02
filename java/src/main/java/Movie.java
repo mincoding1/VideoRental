307eaa589666de8ad32e1d84d9b3f04f4077df3f
mincoding <74457149+mincoding1@users.noreply.github.com>
2025-06-03 01:00:30 +0900

    Update Movie.java

--- java/src/main/java/Movie.java
+++ java/src/main/java/Movie.java
@@ -1,6 +1,6 @@
 public class Movie {
 
-    public static final int CHILDRENS = 2;
+    public static final int CHILDREN = 2;
     public static final int NEW_RELEASE = 1;
     public static final int REGULAR = 0;
 
