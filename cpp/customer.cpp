fda7cde6e6b7e91f1d5e298be5ddd7c32caac041
mincoding <74457149+mincoding1@users.noreply.github.com>
2025-06-03 00:59:31 +0900

    Update customer.cpp

--- cpp/customer.cpp
+++ cpp/customer.cpp
@@ -47,7 +47,7 @@ public:
             case Movie::NEW_RELEASE:
                 thisAmount += each.getDaysRented() * 3;
                 break;
-            case Movie::CHILDRENS:
+            case Movie::CHILDREN:
                 thisAmount += 1.5;
                 if (each.getDaysRented() > 3)
                     thisAmount += (each.getDaysRented() - 3) * 1.5;
